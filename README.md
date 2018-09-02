5066ac96f25b1583acd53f3ff1ab8780ebefed7d
hirotaka.koga <i.feeling.song+git@gmail.com>
2018-09-02 23:44:08 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Ktor Sample
+
+## Debug
+
+src/application.kt を右クリック
+
+
+デバッグ〜 を選択
+
+HelloWorld
+http://localhost:8080/
+
+HTML-DSL
+http://localhost:8080/html-dsl
+
---
+++ src/application.kt
@@ -0,0 +1,91 @@
+package jp.kght6123.ktor.example
+
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import io.ktor.http.*
+import io.ktor.html.*
+import kotlinx.html.*
+import kotlinx.css.*
+import io.ktor.gson.*
+import io.ktor.features.*
+import io.ktor.client.*
+import io.ktor.client.engine.jetty.*
+import io.ktor.http.content.resources
+import io.ktor.http.content.static
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.DevelopmentEngine.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+fun Application.module() {
+    install(ContentNegotiation) {
+        gson {
+        }
+    }
+
+    val client = HttpClient(Jetty) {
+    }
+
+    routing {
+        get("/") {
+            call.respondText("HELLO WORLD!!!!", contentType = ContentType.Text.Plain)
+        }
+
+        get("/html-dsl") {
+            call.respondHtml {
+                head {
+                    styleCss {
+                        // 埋め込みCSS
+                    }
+                    link { rel = "stylesheet"; type = "text/css"; href = "/styles.css" }
+                }
+                body {
+                    h1 { +"HTML" }
+                    p { classes = hashSetOf("myclass"); text("HTML-DSL Hello World!!!") }
+                    ul {
+                        for (n in 1..10) {
+                            li { style { color = Color.blue }; text("$n") }
+                        }
+                    }
+                }
+            }
+        }
+
+        get("/styles.css") {
+            call.respondCss {
+                body {
+                    backgroundColor = Color.red
+                }
+                p {
+                    fontSize = 2.em
+                }
+                rule("p.myclass") {
+                    color = Color.green
+                }
+            }
+        }
+
+        // Static feature. Try to access `/static/ktor_logo.svg`
+        static("/static") {
+            resources("static")
+        }
+
+        get("/json/gson") {
+            call.respond(mapOf("hello" to "world"))
+        }
+    }
+}
+
+fun FlowOrMetaDataContent.styleCss(builder: CSSBuilder.() -> Unit) {
+    style(type = ContentType.Text.CSS.toString()) {
+        CSSBuilder().apply(builder).toString()
+    }
+}
+
+fun CommonAttributeGroupFacade.style(builder: CSSBuilder.() -> Unit) {
+    this.style = CSSBuilder().apply(builder).toString().trim()
+}
+
+suspend inline fun ApplicationCall.respondCss(builder: CSSBuilder.() -> Unit) {
+    this.respondText(CSSBuilder().apply(builder).toString(), ContentType.Text.CSS)
+}
